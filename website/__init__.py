17705e3042704e2da1fb3c5beb284ddaf1e786ec
William Bazil <williambazil12@gmail.com>
2026-04-19 21:45:30 +0300

    initial commit to github

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from os import path, getenv  # Import getenv
+from dotenv import load_dotenv # Import load_dotenv
 from flask_login import LoginManager
 
+# Load the .env file
+load_dotenv()
+
 db = SQLAlchemy()
-DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'mwanza_pilot_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = getenv('SECRET_KEY', 'mwanza_pilot_key')
+
+    # Construct TiDB Connection String
+    DB_USER = getenv('DB_USER')
+    DB_PASS = getenv('DB_PASSWORD')
+    DB_HOST = getenv('DB_HOST')
+    DB_PORT = getenv('DB_PORT')
+    DB_NAME = getenv('DB_NAME')
+
+    # TiDB Connection String using pymysql
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?ssl_verify_cert=true"
+    
+    # Extra TiDB configuration for stability
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        "connect_args": {"ssl": {"fake_config": True}}, # Simplifies SSL for TiDB Cloud
+        "pool_pre_ping": True,
+    }
+
     db.init_app(app)
 
     from .views import views
